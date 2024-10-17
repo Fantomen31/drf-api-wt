fdb2301184468b84a1c4ef2e992e59c355bad196
The_phantom <Neocoinkitt@gmail.com>
2024-10-17 16:14:27 +0000

    added JWT_AUTH_SAMESITE = 'Lax' to resolve deployment bug

--- drfapi/settings.py
+++ drfapi/settings.py
@@ -43,6 +43,7 @@ if 'DEV' not in os.environ:
     ]
 
 REST_USE_JWT = True
+JWT_AUTH_SAMESITE = 'Lax' 
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'

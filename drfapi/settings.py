8a96319f92ba79af3e2557461df82e6069de9edf
Kit Walker <Neocoinkitt@gmail.com>
2024-10-22 08:20:16 +0000

    set debug to = false

--- drfapi/settings.py
+++ drfapi/settings.py
@@ -59,7 +59,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-fantomen31-drfapiwt-bnfltaqquxj.ws.codeinstitute-ide.net',
